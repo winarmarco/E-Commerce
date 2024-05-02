74a19781dd56e9244c1b951710dfd1396530667c
Winar Marco <winarmarco@yahoo.com>
2024-05-02 17:58:09 +1000

    Implemented Search and Create category

--- src/app/(admin)/admin/dashboard/page.tsx
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -1,69 +1,211 @@
-import { Input } from "@/components/ui/input";
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 import { DataTable } from "./_components/product_table";
 import { columns } from "./_components/columns";
 import { type Product } from "@prisma/client";
 import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown, CirclePlus, Plus, Search } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+
+const frameworks: {
+  value: string;
+  label: string;
+}[] = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
+const data: Product[] = [
+  {
+    imageURL:
+      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Hello world",
+    description: "lorem ipsum",
+    price: 3000.0,
+    categoryId: "asdasda",
+    id: "asdasda",
+  },
+  {
+    imageURL:
+      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Hello world",
+    description: "lorem ipsum",
+    price: 3000.0,
+    categoryId: "asdasda",
+    id: "asdasda",
+  },
+  {
+    imageURL:
+      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Hello world",
+    description: "lorem ipsum",
+    price: 3000.0,
+    categoryId: "asdasda",
+    id: "asdasda",
+  },
+];
 
 const Dashboard = () => {
-  const data: Product[] = [
-    {
-      imageURL:
-        "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Hello world",
-      description: "lorem ipsum",
-      price: 3000.0,
-      categoryId: "asdasda",
-      id: "asdasda",
-    },
-    {
-      imageURL:
-        "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Hello world",
-      description: "lorem ipsum",
-      price: 3000.0,
-      categoryId: "asdasda",
-      id: "asdasda",
-    },
-    {
-      imageURL:
-        "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Hello world",
-      description: "lorem ipsum",
-      price: 3000.0,
-      categoryId: "asdasda",
-      id: "asdasda",
-    },
-  ];
+  const [searchInput, setSearchInput] = useState("");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string[]>([]);
+
+  const filteredCategory = frameworks.filter((framework) =>
+    framework.value.toLowerCase() == searchInput.toLowerCase()
+  );
+
 
   return (
-    <>
-      <main className="mx-auto mt-[100px] grid w-full max-w-7xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav
-          className="grid gap-4 text-sm text-muted-foreground"
-          x-chunk="dashboard-04-chunk-0"
-        >
-          <Link href="#" className="font-semibold text-primary">
-            PRODUCTS
-          </Link>
-          <Link href="#">OUTDOOR</Link>
-          <Link href="#">LIVING ROOMS</Link>
-          <Link href="#">KITCHEN</Link>
-          <Link href="#">BATHROOMS</Link>
-          <Link href="#">GARDEN</Link>
-        </nav>
-        <div className="flex flex-col gap-y-8">
-          <div className="flex flex-row justify-between">
-            <Input placeholder="Search product" className="mb-4 w-[300px]" />
-            <Link href="/admin/product/add">
-              <Button>Add Product +</Button>
-            </Link>
+    <div className="flex flex-col gap-y-10">
+      <h1 className="text-2xl font-bold">Product</h1>
+      <div className="flex flex-col gap-y-4">
+        <div className="flex flex-row justify-between gap-x-4">
+          <div className="flex h-10 w-full items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
+            <Search className="h-4 w-4" />
+            <input
+              type="number"
+              placeholder="Search Product"
+              className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+            />
           </div>
-          <DataTable columns={columns} data={data} />
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-[300px] justify-between"
+              >
+                {value.length > 0
+                  ? value.toString().length > 20
+                    ? `${value.toString().slice(0, 20)}...`
+                    : value.toString()
+                  : "Select Category..."}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent align="end" className="w-[300px] p-0">
+              <Command>
+                <CommandInput
+                  className=""
+                  placeholder="Search or Create Category..."
+                  onInput={(e) => setSearchInput(e.currentTarget.value)}
+                />
+                <CommandEmpty>No framework found.</CommandEmpty>
+                <CommandGroup>
+                  <CommandList>
+                    {frameworks.map((option) => (
+                      <CommandItem
+                        key={option.value}
+                        value={option.value}
+                        onSelect={(currentValue) => {
+                          setValue((prevValue) => {
+                            if (!prevValue.includes(currentValue)) {
+                              return [...prevValue, currentValue];
+                            } else {
+                              const updatedValue = prevValue.filter(
+                                (val) => val != currentValue,
+                              );
+                              return [...updatedValue];
+                            }
+                          });
+                          // setOpen(false);
+                          // onSelect(currentValue);
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            value.includes(option.value)
+                              ? "opacity-100"
+                              : "opacity-0",
+                          )}
+                        />
+                        {option.label}
+                      </CommandItem>
+                    ))}
+
+                    {searchInput && filteredCategory.length == 0 && (
+                      <CommandItem key={searchInput} value={searchInput} className="font-medium">
+                        {`+ Create '${searchInput}' category`}
+                      </CommandItem>
+                    )}
+                  </CommandList>
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
+          </Popover>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button>
+                <Plus />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-56">
+              <DropdownMenuItem>
+                <Link
+                  href="/admin/product/add"
+                  className="flex w-full items-center justify-between text-base"
+                >
+                  Add Product <Plus className="h-4 w-4" />
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href="/admin/category/add"
+                  className="flex w-full items-center justify-between text-base"
+                >
+                  Add Category <Plus className="h-4 w-4" />
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-      </main>
-    </>
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
   );
 };
 
--- src/app/(admin)/admin/product/add/page.tsx
+++ src/app/(admin)/admin/product/add/page.tsx
@@ -1,9 +1,155 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { type Product } from "@prisma/client";
+import { z } from "zod";
+import { Textarea } from "@/components/ui/textarea";
+import { DollarSign } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type IAddProduct = Omit<Product, "id">;
+
+const AddProductSchema: z.ZodType<IAddProduct> = z.object({
+  name: z.string({}),
+  description: z.string({}),
+  price: z.coerce.number({}),
+  categoryId: z.string({}),
+  imageURL: z.string({}),
+});
 
 const AddProductPage = () => {
-  return (
-    <div>AddProductPage</div>
-  )
-}
+  const form = useForm<z.infer<typeof AddProductSchema>>({
+    resolver: zodResolver(AddProductSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      price: 0,
+      categoryId: "",
+      imageURL: "",
+    },
+  });
 
-export default AddProductPage
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof AddProductSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Name</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} className="w-[256px]" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Type your message here." {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Price</FormLabel>
+              <FormControl>
+                <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
+                  <DollarSign className="h-4 w-4" />
+                  <input
+                    {...field}
+                    type="number"
+                    className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                  />
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="categoryId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={"field.value"}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a verified email to display" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="imageURL"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Category</FormLabel>
+              <FormControl>
+                <Input {...field} type="file" className="w-[250px]" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default AddProductPage;
