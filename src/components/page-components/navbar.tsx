25a8c78a15d9b97a4a2049b0dcf26af7179c3cf8
Winar Marco <winarmarco@yahoo.com>
2024-06-27 12:40:02 +1000

    Added logo and favicon

--- src/components/page-components/navbar.tsx
+++ src/components/page-components/navbar.tsx
@@ -3,6 +3,8 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import Logo from "../logo";
+import { Separator } from "../ui/separator";
 
 interface INavbarItem {
   label: string;
@@ -38,8 +40,9 @@ const NavBarItem: React.FC<{ item: INavbarItem }> = ({ item }) => {
         href={item.url}
         className={isCurrentUrl ? `font-bold` : ""}
       >
-        {item.label}
+        {item.label.toUpperCase()}
       </Link>
+      {isCurrentUrl && <Separator className="bg-gray-700 h-0.5 rounded-sm"/>}
     </li>
   );
 };
@@ -50,21 +53,21 @@ const NavBar = () => {
   return (
     <nav className="fixed top-0 z-20 h-[90px] w-full bg-white shadow-md">
       <div className="mx-auto flex h-full w-full max-w-7xl flex-row items-center justify-center py-4">
-        <div>LOGOs</div>
+        <div><Logo /></div>
 
-        <ul className="ml-auto flex flex-row items-center justify-center gap-x-5">
+        <ul className="ml-auto flex flex-row items-center justify-center gap-x-8">
           {NAVBAR_ITEMS.map((item) => (
             <NavBarItem key={item.label} item={item} />
           ))}
 
           {session.status === "authenticated" && (
             <Link href="/api/auth/signout">
-              <Button variant="outline">Sign out</Button>
+              <Button variant="outline">SIGN OUT</Button>
             </Link>
           )}
           {session.status === "unauthenticated" && (
             <Link href="/api/auth/signin">
-              <Button>Sign in</Button>
+              <Button>SIGN IN</Button>
             </Link>
           )}
         </ul>
